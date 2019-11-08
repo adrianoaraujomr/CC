d5b6e68be8167eb568e80410ed67f47178efedd3
Adriano <adrianoshirost@gmail.com>
2019-11-08 09:04:29 -0300

    Lexemas

--- FE-Compilador/lex.py
+++ FE-Compilador/lex.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 
+import sys
+
 class Token():
 	def __init__(self,ttype,attribute,line,col):
-		self.ttype     = ttype
+		self.name      = ttype
+		# cnst  = constante
+		# ident = identificador
+		# relop = operador relacional
+		# rword = palavra reservada
+		# pont  = pontuação
+		# op    = operador
 		self.attribute = attribute
 		self.line      = line
 		self.col       = col
 
 class STDiagram():
 	def __init__(self,afd):
-		fd      = open(afd,"r")
-		lines   = fd.readlines()
-		self.tt = [] # transition table
+		fd         = open(afd,"r")
+		lines      = fd.readlines()
+		self.tt    = [] # transition table
+		self.final = [] # final states
+
 		for l in lines:
 			aux = {}
 			l   = l.strip("\n")
@@ -34,10 +44,14 @@ class STDiagram():
 				aux[chr(j)] = int(l[i])
 				i += 1
 			self.tt.append(aux)
-#		print(self.tt[0])
-#		print(self.tt[0]['0'])
-#		print(self.tt[0]['-'])
+			if int(l[i]) == 1:
+				self.final.append(len(self.tt) - 1)
+		print(self.final)
 
+	def is_final(self,state):
+		if state in self.final:
+			return True
+		return False
 
 class Tokenizer():
 	def __init__(self,file_name):
@@ -49,22 +63,72 @@ class Tokenizer():
 		fd          = open(self.source_code,"r")
 		self.buffer = fd.read()
 
+		start = None
+		end   = None
 		state = 0
 		line  = 0
 		col   = 0
 
-		for i in self.buffer:
-		# Ignorar espaços tabs e new lines
-			if i == " " or i == "\t":
-				continue
-			elif i == '\n':
-				line += 1
-				col   = 0
-				continue
+		for i in range(len(self.buffer) - 1):
+		# Setar "ponteiros"
+			head = self.buffer[i]
+			prox = self.buffer[i+1]
+#			print(head,"and",prox)
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
+			if start == None and head != " " and head != "\t" and head != "\n":
+				start = i
+			if head == " " or head == "\t":
+				if start != None:
+					if self.transition_table.is_final(state):
+						end = i
+						print("1",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+						state  = 0
+						start  = None
+						end    = None 
+					else:
+						print("1 Error at",line,col)
+						break
+				else:
+					continue
+			elif head == '\n':
+				if start != None:
+					if self.transition_table.is_final(state):
+						end = i
+						print("2",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+						state  = 0
+						start  = None
+						end    = None
+					else:
+						print("2 Error at",line,col)
+						break
+					line += 1
+					col   = 0
+				else:
+					continue
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
 		# Usar diagrama para determinar estados
-			new_state = self.transition_table[state][i]
-			print(state,"|",new_state,"|",i)
-			print(i," [",line,";",col,"]")
+#			print(state,end=" -> ")
+			state     = self.transition_table.tt[state][head]
+#			print(state)
+			if self.transition_table.is_final(state):
+				end = i
+			if prox == " " or prox == "\t" or prox == "\n":
+				new_state = -1
+			else:
+				new_state = self.transition_table.tt[state][prox]
+#			print(prox,"+",state,"=",new_state)
+
+			# Reconheceu o mais longo
+			if new_state == -1:
+				if end != None: # state hit final at some point
+					print("3",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+					state  = 0
+					start  = None
+					end    = None
+#					lexema = Token("",self.buffer[start:head + 1],line,col)
+				else :
+					print("3 Error at",line,col)
+					
 			col += 1
 
 
