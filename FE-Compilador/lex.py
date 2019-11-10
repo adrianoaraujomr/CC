e11b84597a417abd950ac9cb19acfbfab3f190c4
Adriano <adrianoshirost@gmail.com>
2019-11-10 13:35:53 -0300

    Version 1

--- FE-Compilador/lex.py
+++ FE-Compilador/lex.py
@@ -14,18 +14,20 @@ class Token():
 		self.attribute = attribute
 		self.line      = line
 		self.col       = col
+	def print_token(self):
+		print(self.name,self.attribute,self.line,self.col)
 
 class STDiagram():
 	def __init__(self,afd):
 		fd         = open(afd,"r")
 		lines      = fd.readlines()
 		self.tt    = [] # transition table
-		self.final = [] # final states
+		self.final = {} # final states
 
 		for l in lines:
 			aux = {}
 			l   = l.strip("\n")
-			l   = l.split(";")
+			l   = l.split(",")
 			i   = 1
 
 			for j in range(39,46):
@@ -44,12 +46,15 @@ class STDiagram():
 				aux[chr(j)] = int(l[i])
 				i += 1
 			self.tt.append(aux)
-			if int(l[i]) == 1:
-				self.final.append(len(self.tt) - 1)
+			if l[i] != "0":
+				self.final[str(len(self.tt) - 1)] = l[i]
+#				aux['class'] = l[i]
+#				self.final.append(len(self.tt) - 1)
+
 		print(self.final)
 
 	def is_final(self,state):
-		if state in self.final:
+		if str(state) in self.final.keys():
 			return True
 		return False
 
@@ -81,7 +86,9 @@ class Tokenizer():
 				if start != None:
 					if self.transition_table.is_final(state):
 						end = i
-						print("1",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+						tk = Token(self.transition_table.final[str(state)],self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""),line,col)
+						tk.print_token()
+#						print("1",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
 						state  = 0
 						start  = None
 						end    = None 
@@ -94,7 +101,9 @@ class Tokenizer():
 				if start != None:
 					if self.transition_table.is_final(state):
 						end = i
-						print("2",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+						tk = Token(self.transition_table.final[str(state)],self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""),line,col)
+						tk.print_token()
+#						print("2",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
 						state  = 0
 						start  = None
 						end    = None
@@ -121,7 +130,9 @@ class Tokenizer():
 			# Reconheceu o mais longo
 			if new_state == -1:
 				if end != None: # state hit final at some point
-					print("3",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
+					tk = Token(self.transition_table.final[str(state)],self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""),line,col)
+					tk.print_token()
+##					print("3",self.buffer[start:end + 1].replace(" ","").replace("\n","").replace("\t",""))
 					state  = 0
 					start  = None
 					end    = None
